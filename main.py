f6a6e8f667f55d7d1247cf9a039413dab91bae5d
atharva <113245246+atharvanihalani@users.noreply.github.com>
2024-05-05 19:28:10 -0400

    ff done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 class DenseLayer():
     '''
@@ -13,16 +14,16 @@ class DenseLayer():
     def call(self, input):
         out = np.matmul(input, self.weights)
         out = np.add(out, self.biases)
-        out = self.activation(out)
+        out = np.array(self.activation(out))
         return out
 
     def leaky_relu(self, input):
-        out = [val if val > 0 else 0.1*val for val in input]
+        out = [[val if val > 0 else 0.1*val for val in row] for row in input]
         return out
     
     def softmax(self, input):
         out = np.exp(input)
-        denom = np.sum(out)
+        denom = np.reshape(np.sum(out, 1), (-1, 1))
         out = out / denom
         return out
 
@@ -37,5 +38,25 @@ class Model():
     
     def call(self, inputs):
         '''inputs: batch size X 28*28'''
-        
+        next = inputs
+        for layer in self.layers:
+            next = layer.call(next)
+
         pass
+        preds = np.argmax(next, 1)
+        return preds
+
+def main():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train = np.reshape(x_train, (60000, 28*28))
+    x_train = x_train / 256
+    x_test = np.reshape(x_test, (10000, 28*28))
+    x_test = x_test / 256
+
+
+    model = Model()
+    test_out = model.call(x_train)
+    pass
+    
+
+main()
